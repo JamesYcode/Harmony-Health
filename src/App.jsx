dd70fd1ddb68e67685fdfc76304860ea5db55cb1
James <JamesYcode@gmail.com>
2025-09-01 14:44:55 -0400

    added more styles and section for testimony and added seeded data.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,50 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 
+const data = [
+  {
+    image: {
+      profile: '/src/assets/images/patient_1.jpg',
+    },
+    name: 'John',
+    testimony:
+      'The doctors at Harmony Health truly listened to my concerns and created a treatment plan that worked for me.',
+    id: 1,
+  },
+  {
+    image: {
+      profile: '/src/assets/images/patient_2.jpg',
+    },
+    name: 'Jennifer',
+    testimony:
+      'From the moment I walked in, the staff made me feel comfortable and cared for.',
+    id: 2,
+  },
+  {
+    image: {
+      profile: '/src/assets/images/patient_3.jpg',
+    },
+    name: 'Amber',
+    testimony:
+      'Scheduling an appointment was easy, and I was able to see a provider the same week.',
+    id: 3,
+  },
+  {
+    image: {
+      profile: '/src/assets/images/patient_4.jpg',
+    },
+    name: 'Lin',
+    testimony:
+      'Thanks to Harmony Health, I feel healthier, supported, and more confident in my care.The doctors at Harmony Health truly listened to my concerns and created a treatment plan that worked for me.',
+    id: 4,
+  },
+];
+
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <Home data={data} />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,49 @@
 import './home.css';
 
-function Home() {
+function Home({ data }) {
+  const randomTestimony = data[Math.floor(Math.random() * data.length)];
+
   return (
     <div className='container section '>
       <div className='home'>
         <div className='blob blob-main'></div>
         <div className='blob2 blob-main'></div>
-        <div className='hero-content'>
-          <h2>
-            Excellence in Care
-            <br />
-            <span className='hero'>Commitment to Health</span>
-          </h2>
-        </div>
+        <div className='hero'>
+          <div className='hero-content'>
+            <h2>
+              Excellence in Care
+              <br />
+              <span className='hero-heading'>Commitment to Health</span>
+            </h2>
+          </div>
 
-        <div className='sub-hero'>
-          <p>
-            At Harmony Health, we are dedicated to providing compassionate,
-            patient-centered care. Our team of experienced providers works with
-            you to promote healing, wellness, and peace of mind.
-          </p>
+          <div className='sub-hero'>
+            <p>
+              At Harmony Health, we are dedicated to providing compassionate,
+              patient-centered care. Our team of experienced providers works
+              with you to promote healing, wellness, and peace of mind.
+            </p>
+            <div className='more-info'>
+              <button>Find doctors</button>
+              <button>More Info</button>
+            </div>
+          </div>
+          <div className='partners-container'></div>
 
-          <div className='find-doctors'>
-            <button>Find doctors</button>
+          <div className='testimony-slide'>
+            <div className='testimony-container'>
+              <img
+                className='testimony-img'
+                src={randomTestimony.image.profile}
+                alt={randomTestimony.name}
+              />
+              <p className='testimony-name'>{randomTestimony.name}</p>
+            </div>
+            <p className='testimony-quote'>
+              <blockquote>"</blockquote>
+              {randomTestimony.testimony}
+              <blockquote>"</blockquote>
+            </p>
           </div>
         </div>
       </div>
